ae12dfe7ddf4968827799e9b192977e262c64601
Paul Martin <p_r_martin@yahoo.com>
2013-01-30 10:58:23 -0500

    bugfix for GitHub issue #1
    The public property name did not match what was defined in the rule
    condition item

--- src/Rules/Conditions/LastModifiedCondition.cs
+++ src/Rules/Conditions/LastModifiedCondition.cs
@@ -11,14 +11,14 @@ namespace Sitecore.SharedSource.VersionPruner.Rules.Conditions
 {
     public class LastModifiedCondition<T> : WhenCondition<T> where T : RuleContext
     {
-        public int Value { get; set; }
+        public int Days { get; set; }
 
         protected override bool Execute(T ruleContext)
         {
             Assert.ArgumentNotNull((object)ruleContext, "ruleContext");
             var version = ruleContext.Item;
 
-            return (DateTime.Now - version.Statistics.Updated).Days > this.Value;
+            return (DateTime.Now - version.Statistics.Updated).Days > this.Days;
         }
     }
 }
